b23b325424e500d1aabf24e961da29db7cb3c6e5
markv <c9tabmat@vasvari.org>
2024-11-26 19:45:53 +0100

    broken piece of shi felköti magát ha rámegyek az osztályzatokra

--- KretaBasicSchoolSystem.Desktop/ViewModels/SchoolGrades/SchoolGradesViewModel.cs
+++ KretaBasicSchoolSystem.Desktop/ViewModels/SchoolGrades/SchoolGradesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,54 @@ namespace KretaBasicSchoolSystem.Desktop.ViewModels.SchoolGrades
 {
     public partial class SchoolGradesViewModel: BaseViewModel
     {
+        private CurrentClassViewModel _currentClassViewModel;
+        private TaughtClassesViewModel _taughtClassesViewModel;
+        private SemesterEndViewModel _semesterEndViewModel;
+        private SchoolYearEndViewModel _schoolYearEndViewModel;
+
+        public SchoolGradesViewModel()
+        {
+            _currentSchoolGradesChildView = new CurrentClassViewModel();
+            _currentClassViewModel = new CurrentClassViewModel();
+            _taughtClassesViewModel = new TaughtClassesViewModel();
+            _semesterEndViewModel = new SemesterEndViewModel();
+            _schoolYearEndViewModel = new SchoolYearEndViewModel();
+        }
+
+        public SchoolGradesViewModel(CurrentClassViewModel currentClassViewModel, TaughtClassesViewModel taughtClassesViewModel, SemesterEndViewModel semesterEndViewModel, SchoolYearEndViewModel schoolYearEndViewModel)
+        {
+            _currentSchoolGradesChildView = currentClassViewModel;
+            _currentClassViewModel = currentClassViewModel;
+            _taughtClassesViewModel = taughtClassesViewModel;
+            _semesterEndViewModel = semesterEndViewModel;
+            _schoolYearEndViewModel = schoolYearEndViewModel;
+        }
+
         [ObservableProperty]
         private BaseViewModel _currentSchoolGradesChildView;
+
+        [RelayCommand]
+        public void ShowCurrentClassView()
+        {
+            CurrentSchoolGradesChildView = _currentClassViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowTaughtClassesView()
+        {
+            CurrentSchoolGradesChildView = _taughtClassesViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSemesterEndView()
+        {
+            CurrentSchoolGradesChildView = _semesterEndViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolYearEndView()
+        {
+            CurrentSchoolGradesChildView = _schoolYearEndViewModel;
+        }
     }
 }
